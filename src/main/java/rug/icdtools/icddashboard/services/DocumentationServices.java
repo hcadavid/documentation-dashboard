fe8e9e6dc343f72d66f47f8c735233dda535287f
Hector Cadavid <hector.cadavid@gmail.com>
2022-02-21 21:20:36 +0100

    Redis hierarchies redefined

--- src/main/java/rug/icdtools/icddashboard/services/DocumentationServices.java
+++ src/main/java/rug/icdtools/icddashboard/services/DocumentationServices.java
@@ -23,99 +23,99 @@ import rug.icdtools.icddashboard.models.PublishedICDMetadata;
  */
 @Service
 public class DocumentationServices {
-    
-    @Autowired
-    private RedisTemplate<String,Object> redisTemplate;
-    
-    @Autowired
-    private RedisTemplate<String,PipelineFailureDetails> failureDetailsRedisTemplate;
-    
-    @Autowired
-    private RedisTemplate<String,PipelineFailure> failuresRedisTemplate;
-    
-    @Autowired
-    private RedisTemplate<String,ICDDescription> icdDescriptionRedisTemplate;
-    
-    
-    
 
-    
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, PipelineFailureDetails> failureDetailsRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, PipelineFailure> failuresRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, ICDDescription> icdDescriptionRedisTemplate;
+
     /**
-     * 
+     *
      * @param icdid
      * @param metadata
-     * @return 
+     * @return
      */
-    public PublishedICDMetadata updateCurrentlyPublishedICD (String icdid, PublishedICDMetadata metadata) {
+    public PublishedICDMetadata updateCurrentlyPublishedICD(String icdid, PublishedICDMetadata metadata) {
 
         List<Object> txResults = redisTemplate.execute(new SessionCallback<List<Object>>() {
             @Override
             public List<Object> execute(RedisOperations operations) throws DataAccessException {
                 operations.multi();
                 operations.opsForSet().add("icdids", icdid);
-                operations.opsForValue().set("published:"+icdid, metadata);        
+                operations.opsForValue().set("published:" + icdid, metadata);
                 return operations.exec();
             }
         });
-                
+
         return metadata;
-    }    
-    
+    }
+
     /**
-     * 
+     *
      * @param icdid
      * @param desc
      * @param pipelineid
-     * @return 
+     * @return
      */
     public PipelineFailureDetails registerFailedPipeline(String icdid, PipelineFailureDetails desc, String pipelineid) {
+
+        boolean firstPipelineFailure = !redisTemplate.opsForSet().isMember("failed_pipelines:" + icdid, pipelineid);
+        
 
         List<Object> txResults = redisTemplate.execute(new SessionCallback<List<Object>>() {
             @Override
             public List<Object> execute(RedisOperations operations) throws DataAccessException {
                 operations.multi();
-
-                //add to the list of ICD pipelines only once (a list is used to keep the pipelines order)
-                if (!redisTemplate.opsForSet().isMember("failures:" + icdid + ":pipelines", pipelineid)) {
-                    redisTemplate.opsForList().leftPush("failures:" + icdid, new PipelineFailure(desc.getDate(), pipelineid));
+                
+                //register a pipeline date/id in a list (to keep its order) and a set (for isMember evaluation in O(1))
+                //when the first error from such pipeline is reported                
+                if (firstPipelineFailure) {
+                    operations.opsForList().leftPush("failed_pipelines:" + icdid, new PipelineFailure(desc.getDate(), pipelineid));
+                    operations.opsForSet().add("failed_pipelines:" + icdid, pipelineid);
                 }
-
-                redisTemplate.opsForSet().add("failures:" + icdid + ":pipelines", pipelineid);
-                redisTemplate.opsForList().leftPush("failures:" + icdid + ":" + pipelineid, desc);
-                redisTemplate.opsForSet().add("icdids", new ICDDescription(icdid, "Undefined status"));
+                
+                operations.opsForSet().add("icdstatus:"+icdid,new ICDDescription(icdid, ""));
+                
+                      
+                //add the details of the failure
+                operations.opsForList().leftPush("failures:" + icdid + ":" + pipelineid, desc);
+                
 
                 return operations.exec();
             }
         });
-                 
+
         return desc;
 
     }
 
-    
-    public List<PipelineFailureDetails> getFailedPipelineDetails (String icdid, String pipelineid) throws DocumentationServicesException, NonExistingResourceException{
+    public List<PipelineFailureDetails> getFailedPipelineDetails(String icdid, String pipelineid) throws DocumentationServicesException, NonExistingResourceException {
         List<PipelineFailureDetails> docerrors = failureDetailsRedisTemplate.opsForList().range("failures:" + icdid + ":" + pipelineid, 0, -1);
 
-        if (docerrors == null){
+        if (docerrors == null) {
             throw new DocumentationServicesException(String.format("error while accessing pipeline %s resource.", pipelineid));
-        }
-        else if (docerrors.isEmpty()) {
+        } else if (docerrors.isEmpty()) {
             throw new NonExistingResourceException(String.format("pipeline %s: not found", pipelineid));
         } else {
             return docerrors;
         }
     }
 
-    public Set<ICDDescription> getRegisteredICDs(){    
+    public Set<ICDDescription> getRegisteredICDs() {
         Set<ICDDescription> members = icdDescriptionRedisTemplate.opsForSet().members("icdids");
         return members;
     }
 
-    
     public List<PipelineFailure> getFailedPipelines(String icdid) {
-        List<PipelineFailure> pipelineIds = failuresRedisTemplate.opsForList().range("failures:"+icdid,0,-1);
+        List<PipelineFailure> pipelineIds = failuresRedisTemplate.opsForList().range("failed_pipelines:" + icdid, 0, -1);
         return pipelineIds;
     }
-    
-    
+
 }
